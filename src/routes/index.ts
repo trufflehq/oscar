c81041f3ec46a56c3536fca559c725b7a5c68309
Austin Hallock <austinhallock@gmail.com>
2022-12-15 13:23:08 -0800

    replicate broken state first

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -86,7 +86,6 @@ export class RootController extends Controller<"/"> {
         // "@microsoft/fast-element(.*)context\\.js",
 
         "@truffle/distribute", // TODO: move context into @truffle/shared-contexts
-        "@truffle/api", // TODO: urql/core and unbundle just urql core
         "@truffle/utils", // TODO: remove this when mogul-menu stops using rxjs. causes rxjs error in opera
         "@legendapp/state", // HACK: figure out why legend isn't tracking observables in activity banners w/ ?bundle
       ],
